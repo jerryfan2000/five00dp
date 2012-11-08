a5475ef46418f230141279a059ed1e0081b8db39
Nathan Yuen <nathan.yuen@gmail.com>
2012-11-07 23:11:26 -0500

    PxApi imported

--- src/com/nyuen/test_fivehundred/MainActivity.java
+++ src/com/nyuen/test_fivehundred/MainActivity.java
@@ -1,9 +1,16 @@
 package com.nyuen.test_fivehundred;
 
+import com.fivehundredpx.api.PxApi;
+import com.fivehundredpx.api.auth.AccessToken;
+import com.fivehundredpx.api.auth.OAuthAuthorization;
+import com.google.gson.Gson;
 import com.nyuen.test_fivehundred.adapter.ImageAdapter;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
@@ -16,22 +23,73 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         
         test_adapter = new ImageAdapter(this);
+        
+        
+        
+                
         Bitmap[] mPhotos = new Bitmap[4];
         
-        mPhotos[0] = BitmapFactory.decodeResource(getResources(), R.drawable.one);
-        mPhotos[1] = BitmapFactory.decodeResource(getResources(), R.drawable.one);
-        mPhotos[2] = BitmapFactory.decodeResource(getResources(), R.drawable.one);
-        mPhotos[3] = BitmapFactory.decodeResource(getResources(), R.drawable.one);
-        
+        mPhotos[0] = BitmapFactory.decodeResource(getResources(), R.drawable.a);
+        mPhotos[1] = BitmapFactory.decodeResource(getResources(), R.drawable.b);
+        mPhotos[2] = BitmapFactory.decodeResource(getResources(), R.drawable.c);
+        mPhotos[3] = BitmapFactory.decodeResource(getResources(), R.drawable.d);
+//        mPhotos[4] = BitmapFactory.decodeResource(getResources(), R.drawable.a);
+//        mPhotos[5] = BitmapFactory.decodeResource(getResources(), R.drawable.b);
+//        mPhotos[6] = BitmapFactory.decodeResource(getResources(), R.drawable.c);
+//        mPhotos[7] = BitmapFactory.decodeResource(getResources(), R.drawable.d);
         
 //                new BitmapDrawable(getResources(), R.dr);
         test_adapter.setPhotos(mPhotos);
         setListAdapter(this.test_adapter);
-        
+        new loadEventTask().execute();
+        Log.d("500px", "Ran!!");
     }
 
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        getMenuInflater().inflate(R.menu., menu);
 //        return true;
 //    }
+    private class loadEventTask extends AsyncTask<String, Void, Void> {
+        private final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        protected void onPreExecute() {
+            dialog.setTitle("Please wait...");
+            dialog.setMessage("Retrieving data...");
+            dialog.show();
+        }
+
+        protected Void doInBackground(final String... args) {
+            String key = "H44vO2VyWiCcW0RBlZ3EAEJU4o6nuI2MlNCLLDmx";
+            String secret = "Ufi5Logkfwy0uujKpzZj729hQJQlIgUj5gOahmMj";
+     
+            //PxApi pxapi = new PxApi(, key, secret);
+            PxApi pxapi = new PxApi(secret);
+            Log.d("500px", "Ready!!");
+            try {
+                Gson results = new Gson();    
+                Log.d("500px", pxapi.get("https://api.500px.com/v1/photos?feature=popular").toString() );
+            } catch (Exception e) {
+                Log.e("500px", e.toString());
+            }
+            
+            return null;
+        }
+
+        protected void onPostExecute(final Void unused) {
+            //Log.d("Main", "adapter size: " + m_adapter.getCount());
+
+            if (this.dialog.isShowing()) {
+                this.dialog.dismiss();
+            }
+            
+            //if(m_event != null && m_event.size() > 0){
+               //m_adapter.clear();
+//               for(int i=0;i<m_event.size();i++)
+//                   m_adapter.add(m_event.get(i));
+            //}
+          
+        }
+     }
+    
+    
 }
--- src/com/nyuen/test_fivehundred/adapter/ImageAdapter.java
+++ src/com/nyuen/test_fivehundred/adapter/ImageAdapter.java
@@ -24,11 +24,11 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     public int getCount() {
-        return mPhotos.length;
+        return 1;
     }
 
     public Object getItem(int position) {
-        return mPhotos[position]; 
+        return position; 
     }
     
     public void setPhotos(Bitmap[] photos) {
@@ -39,10 +39,16 @@ public class ImageAdapter extends BaseAdapter {
         return position;    
     }
 
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+    
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageHolder holder;
         if(convertView == null) {
             convertView = mInflater.inflate(R.layout.two_by_two, null, false);
+//            convertView = mInflater.inflate(R.layout.relative_two_by_two, null, false);
             holder = new ImageHolder();
             holder.view = new ImageView[4];
             holder.view[0] = (ImageView) convertView.findViewById(R.id.imageView1);
@@ -50,12 +56,18 @@ public class ImageAdapter extends BaseAdapter {
             holder.view[2] = (ImageView) convertView.findViewById(R.id.imageView3);
             holder.view[3] = (ImageView) convertView.findViewById(R.id.imageView4);
             
+//            holder.view[0] = (ImageView) convertView.findViewById(R.id.imageView5);
+//            holder.view[1] = (ImageView) convertView.findViewById(R.id.imageView6);
+//            holder.view[2] = (ImageView) convertView.findViewById(R.id.imageView7);
+//            holder.view[3] = (ImageView) convertView.findViewById(R.id.imageView8);
+//            
+            
             convertView.setTag(holder);
         } else {
             holder = (ImageHolder) convertView.getTag();
         }
         
-        for(int i = 0; i < getCount(); i++ ) {
+        for(int i = 0; i < mPhotos.length; i++ ) {
             holder.view[i].setImageBitmap(mPhotos[i]);
             //holder.view[i].setLayoutParams(new LayoutParams(holder.view[i].getMeasuredWidth(), holder.view[i].getMeasuredWidth()));
         }
